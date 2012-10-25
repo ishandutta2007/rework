628bd2c4b8fe8498d872f69f2c641806e47e4948
Nicole Deflaux <nicole.deflaux@gmail.com>
2012-10-25 22:43:11 +0000

    Now building separate random forest models for each zone plus one interaction model with the most recent data.

--- competitions/loadForecasting/randomForestLoadOnly.R
+++ competitions/loadForecasting/randomForestLoadOnly.R
@@ -10,13 +10,27 @@ dim(lh)
 str(lh)
 expect_that(nrow(lh), equals(sum(complete.cases(lh))))
 
-labels <- lh[,1]
-train <- lh[,-1]
+# TODO consider separate models per zone
+# TODO can split into train/test since rf can't use all the training data
+
 
 startTime=date(); startTime
-rf <- randomForest(train, labels, xtest=test, ntree=2000)
+
+# Note, when passing all zones in, we run out of memory.  Instead building a model per zone plus
+# an additional model with most recent data for all zones to catch interactions.  
+numRowsForInteraction = 100000
+rowsForInteraction=seq(nrow(lh)-numRowsForInteraction, nrow(lh), 1)
+rfInteraction <- randomForest(load ~ ., lh[rowsForInteraction,], ntree=500)
+rfInteraction
+
+zoneRFs <- list()
+for (zone in 1:20) {
+  zoneRFs[[zone]] <- randomForest(load ~ ., lh[lh$zone_id==zone,], ntree=2000)
+  zoneRFs[[zone]]
+}
+
 stopTime=date(); stopTime
-rf
+
 
 # predictions <- levels(labels)[rf$test$predicted]
 # write(predictions, file="20121004_rf_benchmark.csv", ncolumns=1)
