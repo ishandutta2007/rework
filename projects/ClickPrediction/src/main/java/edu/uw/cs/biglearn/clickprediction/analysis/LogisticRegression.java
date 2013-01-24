252359ee49c43506274b7908201d5edefee7fd34
Nicole Deflaux <nicole.deflaux@gmail.com>
2013-01-24 14:56:02 -0800

    parts 1.4.2 (b) and (c) are done

--- projects/ClickPrediction/src/main/java/edu/uw/cs/biglearn/clickprediction/analysis/LogisticRegression.java
+++ projects/ClickPrediction/src/main/java/edu/uw/cs/biglearn/clickprediction/analysis/LogisticRegression.java
@@ -3,15 +3,13 @@ package edu.uw.cs.biglearn.clickprediction.analysis;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import javax.print.attribute.standard.DateTimeAtCompleted;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
 
@@ -21,91 +19,8 @@ public class LogisticRegression {
 	static final int LOSS_AVG_INTERVAL = 100;
 	static Logger logger = Logger.getLogger("LogisticRegression");
 
-	/**
-	 * This class represents the weights in the logistic regression model.
-	 * 
-	 * @author haijieg
-	 * 
-	 */
-	public class Weights {
-		double w0;
-		/*
-		 * query.get("123") will return the weight for the feature:
-		 * "token 123 in the query field".
-		 */
-		Map<Integer, Double> wTokens;
-		double wPosition;
-		double wDepth;
-		double wAge;
-		double wGender;
-
-		Map<Integer, Integer> accessTime; // keep track of the access timestamp
-											// of feature weights.
-											// Using this to do delayed
-											// regularization.
-
-		public Weights() {
-			w0 = wAge = wGender = wDepth = wPosition = 0.0;
-			wTokens = new HashMap<Integer, Double>();
-			accessTime = new HashMap<Integer, Integer>();
-		}
-
-		@Override
-		public String toString() {
-			DecimalFormat myFormatter = new DecimalFormat("###.##");
-			StringBuilder builder = new StringBuilder();
-			builder.append("Intercept: " + myFormatter.format(w0) + "\n");
-			builder.append("Depth: " + myFormatter.format(wDepth) + "\n");
-			builder.append("Position: " + myFormatter.format(wPosition) + "\n");
-			builder.append("Gender: " + myFormatter.format(wGender) + "\n");
-			builder.append("Age: " + myFormatter.format(wAge) + "\n");
-			builder.append("Tokens: " + wTokens.toString() + "\n");
-			return builder.toString();
-		}
-
-		/**
-		 * @return the l2 norm of this weight vector.
-		 */
-		public double l2norm() {
-			double l2 = w0 * w0 + wAge * wAge + wGender * wGender + wDepth
-					* wDepth + wPosition * wPosition;
-			for (double w : wTokens.values())
-				l2 += w * w;
-			return Math.sqrt(l2);
-		}
-
-		/**
-		 * @return the l0 norm of this weight vector.
-		 */
-		public int l0norm() {
-			return 4 + wTokens.size();
-		}
-
-		public void runningAverage(Weights currentWeight, int[] tokens,
-				int count) {
-			double impact = 1 / count;
-			this.w0 = this.w0 - impact * (this.w0 - currentWeight.w0);
-			this.wDepth = this.wDepth - impact
-					* (this.wDepth - currentWeight.wDepth);
-			this.wPosition = this.wPosition - impact
-					* (this.wPosition - currentWeight.wPosition);
-			// Note: we may not have changed the weights for age and gender so
-			// these two updates could be skipped in those cases
-			this.wAge = this.wAge - impact * (this.wAge - currentWeight.wAge);
-			this.wGender = this.wGender - impact
-					* (this.wGender - currentWeight.wGender);
-
-			// Only update averages for tokens in this instance
-			for (int token : tokens) {
-				Double wTokenPrevious = this.wTokens.get(token); // can be null
-				double wTokenCurrent = currentWeight.wTokens.get(token);
-				if (null == wTokenPrevious) {
-					wTokenPrevious = 0.0;
-				}
-				this.wTokens.put(token, wTokenPrevious - impact
-						* (wTokenPrevious - wTokenCurrent));
-			}
-		}
+	static {
+		logger.setLevel(Level.INFO);
 	}
 
 	/**
@@ -115,7 +30,7 @@ public class LogisticRegression {
 	 * @param instance
 	 * @return
 	 */
-	private double computeWeightFeatureProduct(Weights weights,
+	private static double computeWeightFeatureProduct(Weights weights,
 			DataInstance instance) {
 		// Fill in your code here
 		double dotProduct = 0.0;
@@ -148,8 +63,8 @@ public class LogisticRegression {
 	 * @param step
 	 * @param lambda
 	 */
-	private void performDelayedRegularization(int[] tokens, Weights weights,
-			int now, double step, double lambda) {
+	private static void performDelayedRegularization(int[] tokens,
+			Weights weights, int now, double step, double lambda) {
 		// Fill in your code here.
 	}
 
@@ -162,7 +77,7 @@ public class LogisticRegression {
 	 * @param step
 	 * @return the weights for the model.
 	 */
-	public Weights train(DataSet dataset, double lambda, double step,
+	public static Weights train(DataSet dataset, double lambda, double step,
 			ArrayList<Double> AvgLoss) {
 
 		// Fill in your code here. The structure should look like:
@@ -178,6 +93,8 @@ public class LogisticRegression {
 		int count = 0;
 		double lossAccumulator = 0.0;
 		AvgLoss.add(lossAccumulator);
+		logger.info("Training on data in " + dataset.path + " with lambda "
+				+ lambda + " and step size " + step);
 
 		while (dataset.hasNext()) {
 			DataInstance instance = dataset.nextInstance();
@@ -185,7 +102,7 @@ public class LogisticRegression {
 			if (count % LOSS_AVG_INTERVAL == 0) {
 				double avgLoss = lossAccumulator / LOSS_AVG_INTERVAL;
 				AvgLoss.add(avgLoss);
-				logger.info("Trained (lambda:"
+				logger.debug("Trained (lambda:"
 						+ lambda
 						+ ", step:"
 						+ step
@@ -212,12 +129,13 @@ public class LogisticRegression {
 				// order for use in the weight update formula?
 				// Is the issue log likelihood see Equation from page 29 of
 				// http://www.cs.washington.edu/education/courses/cse599c1/13wi/slides/perceptron-kernels-sgd-annotated.pdf
-				// versus negative log likelihood in KN p 247 eqn 8.5
+				// versus negative log likelihood in KN p 247 eqn 8.5?
 				double error = instance.clicked - prediction;
 
 				lossAccumulator += Math.pow((error), 2);
 
-				weights.w0 = weights.w0 + step * error; // no reg and assumed x0 = 1 
+				weights.w0 = weights.w0 + step * error; // no reg and assumed x0
+														// = 1
 				weights.wDepth = weights.wDepth
 						+ step
 						* ((-1 * lambda * weights.wDepth) + instance.depth
@@ -226,31 +144,33 @@ public class LogisticRegression {
 						+ step
 						* ((-1 * lambda * weights.wPosition) + instance.position
 								* (error));
-				if (instance.age != DataInstance.MISSING_AGE) { // TODO correct?
+				if (instance.age != DataInstance.MISSING_AGE) {
 					weights.wAge = weights.wAge
 							+ step
 							* ((-1 * lambda * weights.wAge) + instance.age
 									* (error));
 				}
-				if (instance.gender != DataInstance.MISSING_GENDER) { // TODO correct?
+				if (instance.gender != DataInstance.MISSING_GENDER) {
 					weights.wGender = weights.wGender
 							+ step
 							* ((-1 * lambda * weights.wGender) + instance.gender
 									* (error));
 				}
 				for (int token : instance.tokens) {
-					Double tokenWeight = weights.wTokens.get(token); // can be null
+					// Can be null if this is this data instance is the first
+					// time we've seen this token
+					Double tokenWeight = weights.wTokens.get(token);
 					if (null == tokenWeight) {
 						tokenWeight = 0.0;
 					}
-					weights.wTokens.put(token, tokenWeight + step
-							* error);
+					weights.wTokens.put(token, tokenWeight + step * error);
 				}
-				weightAccumulator.runningAverage(weights, instance.tokens, count);
+				weightAccumulator.runningAverage(weights, instance.tokens,
+						count);
 			}
 		}
-		// Do not return the running average of weights for this HW problem
-		return weights; // weightAccumulator; 
+		// Do _not_ return the running average of weights for this HW problem
+		return weights; // weightAccumulator;
 	}
 
 	/**
@@ -260,7 +180,7 @@ public class LogisticRegression {
 	 * @param dataset
 	 * @return An array storing the CTR for each datapoint in the test data.
 	 */
-	public ArrayList<Double> predict(Weights weights, DataSet dataset) {
+	public static ArrayList<Double> predict(Weights weights, DataSet dataset) {
 		ArrayList<Double> predictions = new ArrayList<Double>();
 
 		while (dataset.hasNext()) {
@@ -270,7 +190,8 @@ public class LogisticRegression {
 		return predictions;
 	}
 
-	private double computePrediction(Weights weights, DataInstance instance) {
+	private static double computePrediction(Weights weights,
+			DataInstance instance) {
 		double partialResult = Math.exp(computeWeightFeatureProduct(weights,
 				instance));
 		// TODO is this correct or should it be 1 / (1 + partialResult)
@@ -282,6 +203,5 @@ public class LogisticRegression {
 	public static void main(String args[]) throws IOException {
 		// Fill in your code here
 		System.out.println("See unit tests for answers to homework questions");
-
 	}
 }
--- projects/ClickPrediction/src/test/java/edu/uw/cs/biglearn/clickprediction/analysis/LogisticRegressionTest.java
+++ projects/ClickPrediction/src/test/java/edu/uw/cs/biglearn/clickprediction/analysis/LogisticRegressionTest.java
@@ -3,6 +3,8 @@ package edu.uw.cs.biglearn.clickprediction.analysis;
 import static org.junit.Assert.*;
 
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.apache.log4j.BasicConfigurator;
@@ -11,7 +13,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import edu.uw.cs.biglearn.clickprediction.analysis.LogisticRegression.Weights;
 import edu.uw.cs.biglearn.clickprediction.util.EvalUtil;
 
 public class LogisticRegressionTest {
@@ -21,7 +22,7 @@ public class LogisticRegressionTest {
 	static final String DATA_PATH = "/Users/deflaux/rework/projects/ClickPrediction/data/";
 
 	static boolean debug = false; // true;
-	static boolean printAssertions = true;
+	static boolean printAssertions = false; // true;
 	static int debugSize = 100;
 	static DataSet training;
 	static DataSet testing;
@@ -44,33 +45,82 @@ public class LogisticRegressionTest {
 	}
 
 	@Test
-	public void test() {
-		LogisticRegression lr = new LogisticRegression();
+	public void test_homework_1_4_2() throws IOException {
 		double lambda = 0.0;
-		double step = 0.05;
-		ArrayList<Double> avgLoss = new ArrayList<Double>();
-		Weights weights = lr.train(training, lambda, step, avgLoss);
-		//System.out.println("Weights: " + weights);
-		//System.out.println("Average Loss: " + avgLoss);
-		assertEqualsHelper("1.4.2 (b) l2 norm of weights", 0.13261375920128926,
-				weights.l2norm(), DELTA);
+		double steps[] = { 0.001, 0.01, 0.05 };
 
-		ArrayList<Double> predictions = lr.predict(weights, testing);
-		assertEqualsHelper("1.4.2 (b) RMSE of predicted CTR", 0.3022793809306685,
-				EvalUtil.eval(DATA_PATH + "test_label.txt", predictions), DELTA);
-
-		assertEqualsHelper("1.4.2 (b) RMSE of baseline CTR", 0.3022793809306685,
+		assertEqualsHelper("1.4.2 (b) RMSE of baseline CTR",
+				0.1730837340117073,
 				EvalUtil.evalBaseLine(DATA_PATH + "test_label.txt", 0.033655),
 				DELTA);
-	}
-	
-	void assertEqualsHelper(String testCase, Double expected, Double actual, Double delta) {
-		if(true == printAssertions) {
-			logger.info("Test case: " + testCase + ", expected: " + expected + ", actual: " + actual);
+
+		for (double step : steps) {
+			ArrayList<Double> avgLosses = new ArrayList<Double>();
+			Weights weights = LogisticRegression.train(training, lambda, step,
+					avgLosses);
+			
+			// Write losses to a file for later plotting
+			FileWriter writer = new FileWriter(step + "output.csv"); 
+			for(Double avgLoss : avgLosses) {
+			  writer.write(avgLoss.toString());
+			}
+			writer.close();
+
+			double l2norm = weights.l2norm();
+			ArrayList<Double> predictions = LogisticRegression.predict(weights,
+					testing);
+			double rmse = EvalUtil.eval(DATA_PATH + "test_label.txt",
+					predictions);
+
+			if (0.001 == step) {
+				assertEqualsHelper("1.4.2 (b) l2 norm of weights for step size 0.001",
+						3.8018789742819346, l2norm, DELTA);
+
+				assertEqualsHelper("1.4.2 (b) RMSE of predicted CTR for step size 0.001",
+						0.17117218852652383, rmse, DELTA);
+			}
+			else if (0.01 == step) {
+				assertEqualsHelper("1.4.2 (b) l2 norm of weights for step size 0.01",
+						9.057169765210123, l2norm, DELTA);
+
+				assertEqualsHelper("1.4.2 (b) RMSE of predicted CTR for step size 0.01",
+						0.1713282869636794, rmse, DELTA);
+				assertEqualsHelper("1.4.2 (c) weight for intercept for step size 0.01",
+						-2.429819110781506, weights.w0, DELTA);
+				assertEqualsHelper("1.4.2 (c) weight for depth for step size 0.01",
+						0.17221938765761013, weights.wDepth, DELTA);
+				assertEqualsHelper("1.4.2 (c) weight for position for step size 0.01",
+						-0.7735761366536621, weights.wPosition, DELTA);
+				assertEqualsHelper("1.4.2 (c) weight for age for step size 0.01",
+						-0.040948015124305764, weights.wAge, DELTA);
+				assertEqualsHelper("1.4.2 (c) weight for gender for step size 0.01",
+						0.10413821477220414, weights.wGender, DELTA);
+			}
+			else if (0.05 == step) {
+				assertEqualsHelper("1.4.2 (b) l2 norm of weights for step size 0.05",
+						22.29195102596319, l2norm, DELTA);
+
+				assertEqualsHelper("1.4.2 (b) RMSE of predicted CTR for step size 0.05",
+						0.17340495195443922, rmse, DELTA);
+			}
+			else {
+				fail("this step size is not one requested by the homework problem");
+			}
+			resetData();
 		}
-		else {
+	}
+
+	// n 1.4.3 Regularization part, please set \lambda from 0 to 0.014 spaced by
+	// 0.002. (0, 0.002, 0.004, ..., 0.014).
+
+	void assertEqualsHelper(String testCase, Double expected, Double actual,
+			Double delta) {
+		if (true == printAssertions) {
+			logger.info("Test case: " + testCase + ", expected: " + expected
+					+ ", actual: " + actual);
+		} else {
 			assertEquals(testCase, expected, actual, delta);
 		}
-		
+
 	}
 }
