533c7b6975b92ba10b6af6a65ae17adfe317b26c
Nicole Deflaux <nicole.deflaux@gmail.com>
2013-02-02 11:22:16 -0800

    (1) Now stopping when improvement in centroids is less than a certain value (2) Fixed an issue where for k >= 6 the graph would not plot shaped points

--- projects/Clustering/kmeans.R
+++ projects/Clustering/kmeans.R
@@ -4,17 +4,19 @@ library(foreach)
 library(testthat)
 library(ggplot2)
 
-data <- read.csv('/Users/deflaux/rework/projects/Clustering/data/2DGaussianMixture.csv', header=TRUE)
-str(data)
-expect_that(ncol(data), equals(3))
-data <- cbind(data, cluster=as.factor(rep(-1, nrow(data))))
+setwd('/Users/deflaux/rework/projects/Clustering')
+
+origData <- read.csv('/Users/deflaux/rework/projects/Clustering/data/2DGaussianMixture.csv', header=TRUE)
+str(origData)
+expect_that(ncol(origData), equals(3))
+data <- cbind(origData, cluster=as.factor(rep(-1, nrow(origData))))
 str(data)
 expect_that(ncol(data), equals(4))
 
 plotClusters <- function(data, centroids) {
-    p <- ggplot(data=data, aes(x=x1, y=x2, color=cluster, shape=cluster))
+    p <- ggplot(data=data, aes(x=x1, y=x2, color=cluster))
     p <- p + geom_point() + geom_point(data=centroids, aes(x=x1, y=x2, color=cluster), size=5)
-    p <- p + geom_point(data=centroids, aes(x=x1, y=x2, color=cluster), shape=19, size=52, alpha=.3, show_guide=FALSE)
+    p <- p + geom_point(data=centroids, aes(x=x1, y=x2, color=cluster), shape=19, size=52, alpha=.5, show_guide=FALSE)
     p
 }
 
@@ -23,11 +25,11 @@ llyodsKmeans <- function(k, data, delta) {
     centroids = data[sample(nrow(data),k),c('x1','x2')]
     centroids <- cbind(centroids, cluster=as.factor(seq(1,k)))
 
-    prevCost = 0.0
-    currentCost = 0.0
+    prevCost = NA
+    currentCost = NA
+    iteration = 0
     
-    #while(TRUE) {
-    for(i in 1:10) {
+    while(TRUE) {
         # E Step: Assign each data point to the closest cluster
         clusters = apply(data[,c('x1','x2')], 
                          1, 
@@ -43,10 +45,28 @@ llyodsKmeans <- function(k, data, delta) {
         # M Step: Compute the average of all points assigned to each centroid and then update each
         newcentroids = sapply(seq(1:k), function(c) { colSums(data[data$cluster==c,c('x1','x2')])/nrow(data[data$cluster==c,c('x1','x2')]) })
         centroids[,c('x1','x2')] = t(newcentroids)
+        
+        # Compute Cost
+        costs <- sapply(seq(1:k), 
+                        function(c) { sum(
+                            apply(data[data$cluster==c,c('x1','x2')], 
+                                  1, 
+                                  function(x) { (x - centroids[c,c('x1','x2')])^2 })) })
+        currentCost = sum(costs)
+        if(!is.na(prevCost)) {
+            if(delta > (prevCost - currentCost)) {
+                break;
+            }
         }
+        prevCost = currentCost
+        iteration = iteration+1
+        print(paste("NumClusters:", k, "Iteration:", iteration))
+        expect_that(data[,c('x1','x2')], equals(origData[,c('x1','x2')]))
+    }
     
     plotClusters(data, centroids)
     ggsave(file=paste(k,'kmeans.pdf',sep='_'))
+    iteration
 }
 
 
@@ -55,4 +75,7 @@ set.seed(42)
 registerDoMC()
 getDoParWorkers()
 mcoptions <- list(preschedule=FALSE, set.seed=FALSE)
-foreach(k=c(2, 3, 5, 10, 15, 20), .options.multicore=mcoptions) %dopar% llyodsKmeans(k=k, data=data, delta=0.0)
+foreach(k=c(2, 3, 5, 10, 15, 20), .options.multicore=mcoptions) %dopar% llyodsKmeans(k=k, data=data, delta=0.01)
+
+#debug(llyodsKmeans)
+#llyodsKmeans(k=15, data=data, delta=0.01)
