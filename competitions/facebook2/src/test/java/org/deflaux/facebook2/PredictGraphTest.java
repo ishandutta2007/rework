b3cc5da07a2feec4990f18c18a524d9dc1edb073
Nicole Deflaux <nicole.deflaux@gmail.com>
2013-03-14 21:16:47 -0700

    Now tuning parameters based upon graph predictions.

--- competitions/facebook2/src/test/java/org/deflaux/facebook2/PredictGraphTest.java
+++ competitions/facebook2/src/test/java/org/deflaux/facebook2/PredictGraphTest.java
@@ -86,69 +86,92 @@ public class PredictGraphTest {
 
 	@Test
 	public void testPredictGraph() throws IOException {
-		ExistenceModel existenceModel = new ExistenceModel(0.05, 0.1,
+		final double steps[] = { 0.001, 0.005, 0.01, 0.05 };
+		final double lambdas[] = { 0.0, 0.002, 0.012, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6 };
+		
+		CostModel costModel = new CostModel(0.05, 0.001,
 				historyWindowSize, numDimensions);
-		CostModel costModel = new CostModel(0.05, 0.001, historyWindowSize,
-				numDimensions);
-
-		DataInstance instance = null;
+		boolean costModelTrained = false;
+		
 		Stopwatch watch = new Stopwatch();
-		while (training.hasNext()) {
-			instance = training.nextInstance(instance, numDimensions);
-			if (11 == training.getEpoch()) {
-				// Do not train with data from epoch 11 onward
-				break;
-			}
-			if (!instance.isValid()) {
-				continue;
-			}
-			existenceModel.train(instance);
-			costModel.train(instance);
-		}
-		logger.info("Time: " + watch.elapsedTime());
+		for (double lambda : lambdas) {
+			for (double step : steps) {
 
-		// Instead of a n^2 loop to predict all edges, just predict for edges we
-		// have seen in the past plus edges in the test set
-		for (int epoch = 11; epoch <= 15; epoch++) {
-			String filePathPredictedGraph = "/Users/deflaux/rework/competitions/facebook2/data/graph"
-					+ epoch + ".txt";
-			Writer predictedGraph = new BufferedWriter(new FileWriter(
-					filePathPredictedGraph));
+				ExistenceModel existenceModel = new ExistenceModel(step,
+						lambda, historyWindowSize, numDimensions);
 
-			int numEdges = 0;
-			for (String edge : edges) {
-				String[] fields = edge.split("\\|");
-				String tail = fields[0];
-				String head = fields[1];
-				Double existencePrediction = existenceModel.predictEdge(tail,
-						head, epoch);
-				if (EXISTENCE_PREDICTION_THRESHOLD < existencePrediction) {
-					// The edge exists for the graph at this epoch
-					// Remember to write out the rawCost -> zero means free
-					int cost = (COST_PREDICTION_THRESHOLD < costModel
-							.predictEdge(tail, head, epoch)) ? 0 : 1;
-					predictedGraph.write(tail + "|" + head + "|" + cost + "\n");
+				DataInstance instance = null;
+				while (training.hasNext()) {
+					instance = training.nextInstance(instance, numDimensions);
+					if (11 == training.getEpoch()) {
+						// Do not train with data from epoch 11 onward
+						break;
+					}
+					if (!instance.isValid()) {
+						continue;
+					}
+					existenceModel.train(instance);
+					if(!costModelTrained) {
+						costModel.train(instance);
+					}
 				}
-				numEdges++;
-				if (0 == numEdges % 1000) {
-					logger.debug("Progress: " + numEdges / (double) edges.size()
-							+ " with " + numEdges + " edges");
+				logger.info("Time: " + watch.elapsedTime());
+				costModelTrained = true;
+				
+				// Instead of a n^2 loop to predict all edges, just predict for
+				// edges we
+				// have seen in the past plus edges in the test set
+				for (int epoch = 11; epoch <= 15; epoch++) {
+					String filePathPredictedGraph = "/Users/deflaux/rework/competitions/facebook2/data/graph"
+							+ epoch + ".txt";
+					Writer predictedGraph = new BufferedWriter(new FileWriter(
+							filePathPredictedGraph));
+
+					int numEdges = 0;
+					for (String edge : edges) {
+						String[] fields = edge.split("\\|");
+						String tail = fields[0];
+						String head = fields[1];
+						Double existencePrediction = existenceModel
+								.predictEdge(tail, head, epoch);
+						if (EXISTENCE_PREDICTION_THRESHOLD < existencePrediction) {
+							// The edge exists for the graph at this epoch
+							// Remember to write out the rawCost -> zero means
+							// free
+							int cost = (COST_PREDICTION_THRESHOLD < costModel
+									.predictEdge(tail, head, epoch)) ? 0 : 1;
+							predictedGraph.write(tail + "|" + head + "|" + cost
+									+ "\n");
+						}
+						numEdges++;
+						if (0 == numEdges % 1000) {
+							logger.debug("Progress: " + numEdges
+									/ (double) edges.size() + " with "
+									+ numEdges + " edges");
+						}
+					}
+					predictedGraph.close();
+
+					String filePathActualGraph = "/Users/deflaux/rework/competitions/facebook2/data/normTrain"
+							+ epoch + ".txt";
+					ErrorMetrics existenceMetrics = new ErrorMetrics();
+					ErrorMetrics costMetrics = new ErrorMetrics();
+
+					CompareGraphsTest.compareGraphs(filePathActualGraph,
+							filePathPredictedGraph, existenceMetrics,
+							costMetrics);
+
+					System.out.println(epoch + " Existence f-score: "
+							+ existenceMetrics.getFScore() + ", "
+							+ existenceMetrics + ", " + existenceModel);
+					System.out.println(epoch + " Cost f-score: "
+							+ costMetrics.getFScore() + ", " + costMetrics
+							+ ", " + costModel);
 				}
+				logger.info("Time: " + watch.elapsedTime());
+				resetData();
 			}
-			predictedGraph.close();
-
-			String filePathActualGraph = "/Users/deflaux/rework/competitions/facebook2/data/normTrain"
-					+ epoch + ".txt";
-			ErrorMetrics existenceMetrics = new ErrorMetrics();
-			ErrorMetrics costMetrics = new ErrorMetrics();
-
-			CompareGraphsTest.compareGraphs(filePathActualGraph,
-					filePathPredictedGraph, existenceMetrics, costMetrics);
-
-			System.out.println(epoch + " Existence f-score: " + existenceMetrics.getFScore() + ", " + existenceMetrics+ ", " + existenceModel);
-			System.out.println(epoch + " Cost f-score: " + costMetrics.getFScore() + ", " + costMetrics + ", " + costModel);
 		}
-		logger.info("Time: " + watch.elapsedTime());
 	}
 
 	static void assertEqualsHelper(String testCase, Object expected,
