1701b28bdbc0cc82e770f160d56bc5a51fef28d4
Nicole Deflaux <nicole.deflaux@gmail.com>
2013-03-29 09:33:55 -0700

    Incorporated corrections to homework 4.  Still need to test this code when I get access to matlab again.

--- projects/CollaborativeFiltering/matlab/NCut.m
+++ projects/CollaborativeFiltering/matlab/NCut.m
@@ -6,7 +6,21 @@ function [ U ] = NCut( W, k )
 I = eye(size(W,1));
 D = diag(sum(W,2));
 Lsym =  I - D^-1/2 * W * D^-1/2;
-[eigV, eigD] = eig(Lsym);
+# TODO test bug fixes using matlab when I have access to matlab again
+[eigV, eigD] = eigs(Lsym);
 U = eigV(:,1:k);
+
+# Per Jay
+# The normalized cut essentially solves the following generalized eigenvalue problem
+
+# (D-W)y = \lambda Dy
+
+# By taking the transform: z = D^{1/2}y, the above problem becomes:
+
+# D^{-1/2}(D-W)D^{-1/2} z = \lambda z
+
+# Once you got z back by doing SVD on D^{-1/2}(D-W)D^{-1/2}, you need to recover y by taking y = D^{-1/2}z.
+
+U = D^{-1/2}U
 
 end
